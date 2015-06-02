79eabbe2fdcf47e44cf5e2722fd03a89bbcbbc01
Patrick Breheny <patrick-breheny@uiowa.edu>
2015-06-02 14:00:11 -0500

    Better double/int type checking for penalty.factor

--- R/ncvreg.R
+++ R/ncvreg.R
@@ -1,5 +1,5 @@
-ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("MCP", "SCAD", "lasso"), 
-                   gamma=switch(penalty, SCAD=3.7, 3), alpha=1, lambda.min=ifelse(n>p,.001,.05), nlambda=100, 
+ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("MCP", "SCAD", "lasso"),
+                   gamma=switch(penalty, SCAD=3.7, 3), alpha=1, lambda.min=ifelse(n>p,.001,.05), nlambda=100,
                    lambda, eps=.001, max.iter=1000, convex=TRUE, dfmax=p+1, penalty.factor=rep(1, ncol(X)),
                    warn=TRUE, returnX=FALSE, ...) {
   ## Error checking
@@ -12,6 +12,7 @@ ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("M
     tmp <- try(y <- as.numeric(y), silent=TRUE)
     if (class(tmp)[1] == "try-error") stop("y must numeric or able to be coerced to numeric")
   }
+  if (storage.mode(penalty.factor) != "double") storage.mode(penalty.factor) <- "double"
   family <- match.arg(family)
   penalty <- match.arg(penalty)
   standardize <- TRUE
@@ -23,11 +24,11 @@ ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("M
   if (any(is.na(y)) | any(is.na(X))) stop("Missing data (NA's) detected.  Take actions (e.g., removing cases, removing features, imputation) to eliminate missing data before passing X and y to ncvreg")
   if (family=="binomial" & length(table(y)) > 2) stop("Attemping to use family='binomial' with non-binary data")
   if (family=="binomial" & !identical(sort(unique(y)), 0:1)) y <- as.numeric(y==max(y))
-  
+
   ## Deprication support
   dots <- list(...)
   if ("n.lambda" %in% names(dots)) nlambda <- dots$n.lambda
-  
+
   ## Set up XX, yy, lambda
   if (standardize) {
     std <- .Call("standardize", X)
@@ -41,7 +42,7 @@ ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("M
     XX <- X
   }
   p <- ncol(XX)
-  
+
   if (family=="gaussian") {
     yy <- y - mean(y)
   } else {
@@ -55,7 +56,7 @@ ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("M
     nlambda <- length(lambda)
     user.lambda <- TRUE
   }
-  
+
   ## Fit
   if (family=="gaussian" & standardize==TRUE) {
     res <- .Call("cdfit_gaussian", XX, yy, penalty, lambda, eps, as.integer(max.iter), as.double(gamma), penalty.factor, alpha, as.integer(dfmax), as.integer(user.lambda | any(penalty.factor==0)))
@@ -81,7 +82,7 @@ ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("M
     loss <- res[[3]]
     iter <- res[[4]]
   }
-  
+
   ## Eliminate saturated lambda values, if any
   ind <- !is.na(iter)
   if (family!="gaussian" | standardize==TRUE) a <- a[ind]
@@ -93,7 +94,7 @@ ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("M
 
   ## Local convexity?
   convex.min <- if (convex & standardize) convexMin(b, XX, penalty, gamma, lambda*(1-alpha), family, penalty.factor, a=a) else NULL
-  
+
   ## Unstandardize
   if (standardize) {
     beta <- matrix(0, nrow=(ncol(X)+1), ncol=length(lambda))
@@ -103,12 +104,12 @@ ncvreg <- function(X, y, family=c("gaussian","binomial","poisson"), penalty=c("M
   } else {
     beta <- if (family=="gaussian") b else rbind(a, b)
   }
-  
+
   ## Names
   varnames <- if (is.null(colnames(X))) paste("V",1:ncol(X),sep="") else colnames(X)
   if (family!="gaussian" | standardize==TRUE) varnames <- c("(Intercept)", varnames)
   dimnames(beta) <- list(varnames, round(lambda,digits=4))
-  
+
   ## Output
   val <- structure(list(beta = beta,
                         iter = iter,
--- R/setupLambda.R
+++ R/setupLambda.R
@@ -1,7 +1,7 @@
 setupLambda <- function(X, y, family, alpha, lambda.min, nlambda, penalty.factor) {
   n <- nrow(X)
   p <- ncol(X)
-  
+
   ## Determine lambda.max
   ind <- which(penalty.factor!=0)
   if (length(ind)!=p) {
@@ -15,13 +15,13 @@ setupLambda <- function(X, y, family, alpha, lambda.min, nlambda, penalty.factor
     zmax <- .Call("maxprod", X, residuals(fit, "working") * fit$weights, ind, penalty.factor) / n
   }
   lambda.max <- zmax/alpha
-  
+
   if (lambda.min==0) {
     lambda <- c(exp(seq(log(lambda.max),log(.001*lambda.max),len=nlambda-1)),0)
   } else {
     lambda <- exp(seq(log(lambda.max),log(lambda.min*lambda.max),len=nlambda))
   }
-  
+
   if (length(ind)!=p) lambda[1] <- lambda[1] * 1.000001
   lambda
 }
