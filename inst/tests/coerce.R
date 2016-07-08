cc094383c4cc8b10c54c2ff626d8355d1cb05bd6
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-07-08 15:31:51 -0500

    A few more tests added to coerce

--- inst/tests/coerce.R
+++ inst/tests/coerce.R
@@ -1,5 +1,16 @@
+.test <- "X data frame, y factor"
 data(heart)
 X <- heart[,1:9]
 y <- factor(heart$chd, labels=c("No", "Yes"))
 fit <- ncvreg(X, y, family="binomial")
-std(X)
+s <- std(X)
+
+.test <- "integer X, y"
+X <- as.matrix(round(X))
+storage.mode(X) <- "integer"
+y <- heart$chd
+fit <- ncvreg(X, y, family="binomial")
+
+.test <- "logical y"
+y <- heart$chd==1
+fit <- ncvreg(X, y, family="binomial")
