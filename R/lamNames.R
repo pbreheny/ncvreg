b9dc019107ea2d9a7e68408a45d7018168e2c75e
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-09-09 10:10:10 -0500

    Bug fix: lamNames with single lambda passed

--- R/lamNames.R
+++ R/lamNames.R
@@ -1,5 +1,9 @@
 lamNames <- function(l) {
-  d <- ceiling(-log10(-max(diff(l))))
-  d <- min(max(d,4), 10)
+  if (length(l) > 1) {
+    d <- ceiling(-log10(-max(diff(l))))
+    d <- min(max(d,4), 10)
+  } else {
+    d <- 4
+  }
   formatC(l, format="f", digits=d)
 }
