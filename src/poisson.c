9f435a90105f0f519bbb552a4c383eb5e3fdaa6d
Congrui Yi <congrui-yi@uiowa.edu>
2016-10-01 22:07:14 -0500

    Update poisson.c

--- src/poisson.c
+++ src/poisson.c
@@ -199,7 +199,7 @@ SEXP cdfit_poisson(SEXP X_, SEXP y_, SEXP penalty_, SEXP lambda, SEXP eps_, SEXP
 	// Scan for violations in strong set
 	int violations = 0;
 	for (int j=0; j<p; j++) {
-	  if (e1[j]==0 & e2[j]==1) {
+	  if (e1[j]==0 && e2[j]==1) {
 	    z[j] = crossprod(X, s, n, j)/n;
 	    l1 = lam[l] * m[j] * alpha;
 	    if (fabs(z[j]) > l1) {
