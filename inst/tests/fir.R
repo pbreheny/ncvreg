f5e573cd55e2c83555e707d915ac1d4ae09113d3
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-12-20 12:22:36 -0600

    Bug fix: fir was crashing for saturated models

--- inst/tests/fir.R
+++ inst/tests/fir.R
@@ -35,6 +35,17 @@ plot(fir(fit))
 plot(fir(fit), type="EF")
 par(op)
 
+n <- 50
+p <- 100
+X <- matrix(rnorm(n*p), n, p)
+y <- rbinom(n, 1, 0.5)
+fit <- ncvreg(X, y, lambda.min=0, family='binomial')
+fir(fit)
+op <- par(mfrow=2:1)
+plot(fir(fit))
+plot(fir(fit), type="EF")
+par(op)
+
 # Cox
 X <- matrix(rnorm(50*10), 50, 10)
 y <- cbind(rexp(50, exp(X[,1])), sample(rep(0:1, c(10,40))))
