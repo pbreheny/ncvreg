d7ac1e2e67a56e15825cb772861dd570f2e30615
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-04-07 09:07:00 -0500

    Changing & to &&

--- src/gaussian.c
+++ src/gaussian.c
@@ -149,7 +149,7 @@ SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP penalty_, SEXP lambda, SEXP eps_, SEX
 	// Scan for violations in strong set
 	int violations = 0;
 	for (int j=0; j<p; j++) {
-	  if (e1[j]==0 & e2[j]==1) {
+	  if (e1[j]==0 && e2[j]==1) {
 
 	    z[j] = crossprod(X, r, n, j)/n;
 
