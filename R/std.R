321b984a67053a2426c717391619e8c6429b92b2
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-02-12 10:38:05 -0600

    Expanding scope of std: now works with integer matrices and vectors

--- R/std.R
+++ R/std.R
@@ -1,7 +1,37 @@
+#' Standardizes a design matrix
+#' 
+#' The function \code{std} accepts a design matrix and returns a standardized version of that matrix (i.e., each column will have mean 0 and mean sum of squares equal to 1).
+#' 
+#'   This function centers and scales each column of \code{X} so that
+#'   \deqn{\sum_{i=1}^n x_{ij}=0}{sum(X[,j])=0}
+#'   and
+#'   \deqn{n^{-1} \sum_{i=1}^n x_{ij}^2 = 1}{mean(X[,j]^2)=1}
+#'   for all j.  This is usually not necessary to call directly, as \code{ncvreg} internally standardizes the design matrix, but inspection of the standardized design matrix can sometimes be useful.  This differs from the base R function \code{\link[base]{scale}} in two ways:
+#'   
+#'   1. `scale` uses the sample standard deviation \code{sqrt(sum(x^2)/(n-1))}, while \code{std} uses the root-mean-square (population) standard deviation \code{sqrt(mean(sum(x^2)))}
+#'   2. `std` is faster.
+#' 
+#' @param X   A matrix (or object that can be coerced to a matrix, such as a data frame or numeric vector).
+#' 
+#' @return The standardized design matrix, with the following attribues:
+#'   * `center`, `scale`: mean and standard deviation used to scale the columns
+#'   * `nonsingular`: A vector indicating which columns of the original design matrix were able to be standardized (constant columns cannot be standardized to have a standard deviation of 1)
+#' 
+#' @examples 
+#' X <- matrix(rnorm(50), 10, 5)
+#' S <- std(X)
+#' apply(S, 2, sum)
+#' apply(S, 2, function(x) mean(x^2))
+
 std <- function(X) {
+  if (typeof(X) == 'integer') storage.mode(X) <- 'double'
   if (!inherits(X, "matrix")) {
-    tmp <- try(X <- model.matrix(~0+., data=X), silent=TRUE)
-    if (inherits(tmp, "try-error")) stop("X must be a matrix or able to be coerced to a matrix", call.=FALSE)
+    if (is.numeric(X)) {
+      X <- matrix(as.double(X), ncol=1)
+    } else {
+      tmp <- try(X <- model.matrix(~0+., data=X), silent=TRUE)
+      if (inherits(tmp, "try-error")) stop("X must be a matrix or able to be coerced to a matrix", call.=FALSE)
+    }
   }
   STD <- .Call("standardize", X)
   dimnames(STD[[1]]) <- dimnames(X)
--- inst/tests/standardize.R
+++ inst/tests/standardize.R
@@ -6,5 +6,22 @@ l <- 5
 .test = "std standardizes correctly"
 X <- matrix(rnorm(n*p),ncol=p)
 XX <- std(X)
-check(apply(XX,2,mean), rep(0,5))
-check(apply(XX,2,crossprod), rep(20,5))
+check(apply(XX, 2, mean), rep(0, ncol(XX)))
+check(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
+
+.test = "std works on data frames"
+XX <- std(airquality)
+check(apply(XX, 2, mean), rep(0, ncol(XX)))
+check(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
+
+.test = "std works with integer matrix"
+X <- matrix(as.integer(rpois(n*p, 1)), ncol=p)
+XX <- std(X)
+check(apply(XX, 2, mean), rep(0, ncol(XX)))
+check(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
+
+.test = "std works with vector input"
+x <- rnorm(n)
+XX <- std(x)
+check(apply(XX, 2, mean), rep(0, ncol(XX)))
+check(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
