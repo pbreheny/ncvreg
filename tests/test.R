10d0262526a436d95fe205a6611c9907e1aac0b7
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-07-10 11:31:06 -0500

    Tests: allow setting R_FORCE_TEST="TRUE" (for covr)

--- tests/test.R
+++ tests/test.R
@@ -1,5 +1,5 @@
 if (requireNamespace("tinytest", quietly=TRUE)) {
-  if (length(unclass(packageVersion("ncvreg"))[[1]]) == 4) {
+  if (length(unclass(packageVersion("ncvreg"))[[1]]) == 4 | Sys.getenv('R_FORCE_TEST') == 'TRUE') {
     tinytest::test_package("ncvreg", pattern="^[^_].*\\.[rR]$")
   }
 }
