52de7fae12f0ea0c6c71b1241f2f4501925a9686
Patrick Breheny <patrick-breheny@uiowa.edu>
2023-03-22 13:11:15 -0500

    std(): Can now be applied to new data

--- R/std.R
+++ R/std.R
@@ -11,39 +11,62 @@
 #'   1. `scale` uses the sample standard deviation \code{sqrt(sum(x^2)/(n-1))}, while \code{std} uses the root-mean-square (population) standard deviation \code{sqrt(mean(sum(x^2)))}
 #'   2. `std` is faster.
 #' 
-#' @param X   A matrix (or object that can be coerced to a matrix, such as a data frame or numeric vector).
+#' @param X     A matrix (or object that can be coerced to a matrix, such as a data frame or numeric vector).
+#' @param Xnew  Optional. If supplied, `X` must be the output of `std()` and `Xnew` is to be standardized in the same way. See examples for why this might be useful.
 #' 
 #' @return The standardized design matrix, with the following attribues:
 #'   * `center`, `scale`: mean and standard deviation used to scale the columns
 #'   * `nonsingular`: A vector indicating which columns of the original design matrix were able to be standardized (constant columns cannot be standardized to have a standard deviation of 1)
 #' 
 #' @examples 
-#' X <- matrix(rnorm(50), 10, 5)
-#' S <- std(X)
+#' data(Prostate)
+#' S <- std(Prostate$X)
 #' apply(S, 2, sum)
 #' apply(S, 2, function(x) mean(x^2))
-#' @export std
+#' 
+#' # Standardizing new observations
+#' X1 <- Prostate$X[1:90,]
+#' X2 <- Prostate$X[91:97,]
+#' S <- std(X1)
+#' head(std(S, X2))
+#' # Useful if you fit to a standardized X, but then get new obs:
+#' y <- Prostate$y[1:90]
+#' fit <- ncvreg(S, y)
+#' predict(fit, std(S, X2), lambda=0.1)
+#' # Same as
+#' predict(ncvreg(X1, y), X2, lambda=0.1)
+#' @export
 
-std <- function(X) {
-  if (typeof(X) == 'integer') storage.mode(X) <- 'double'
-  if (!inherits(X, "matrix")) {
-    if (is.numeric(X)) {
-      X <- matrix(as.double(X), ncol=1)
-    } else {
-      tmp <- try(X <- stats::model.matrix(~0+., data=X), silent=TRUE)
-      if (inherits(tmp, "try-error")) stop("X must be a matrix or able to be coerced to a matrix", call.=FALSE)
+std <- function(X, Xnew) {
+  if (missing(Xnew)) {
+    if (typeof(X) == 'integer') storage.mode(X) <- 'double'
+    if (!inherits(X, "matrix")) {
+      if (is.numeric(X)) {
+        X <- matrix(as.double(X), ncol=1)
+      } else {
+        tmp <- try(X <- stats::model.matrix(~0+., data=X), silent=TRUE)
+        if (inherits(tmp, "try-error")) stop("X must be a matrix or able to be coerced to a matrix", call.=FALSE)
+      }
     }
-  }
-  STD <- .Call("standardize", X)
-  dimnames(STD[[1]]) <- dimnames(X)
-  ns <- which(STD[[3]] > 1e-6)
-  if (length(ns) == ncol(X)) {
-    val <- STD[[1]]
+    STD <- .Call("standardize", X)
+    dimnames(STD[[1]]) <- dimnames(X)
+    ns <- which(STD[[3]] > 1e-6)
+    if (length(ns) == ncol(X)) {
+      val <- STD[[1]]
+    } else {
+      val <- STD[[1]][, ns, drop=FALSE]
+    }
+    attr(val, "center") <- STD[[2]]
+    attr(val, "scale") <- STD[[3]]
+    attr(val, "nonsingular") <- ns
   } else {
-    val <- STD[[1]][, ns, drop=FALSE]
+    attrx <- names(attributes(X))
+    if (!('center' %in% attrx & 'scale' %in% attrx & 'nonsingular' %in% attrx)) {
+      stop('If supplying both X and Xnew, must run std(X) first and pass the result to std', call.=FALSE)
+    }
+    val <- scale(Xnew[,attr(X, 'nonsingular')], attr(X, 'center'), attr(X, 'scale'))
+    attributes(val)[c('nonsingular', 'center', 'scale')] <- attributes(X)[c('nonsingular', 'center', 'scale')]
+    attributes(val)[c('scaled:center', 'scaled:scale')] <- NULL
   }
-  attr(val, "center") <- STD[[2]]
-  attr(val, "scale") <- STD[[3]]
-  attr(val, "nonsingular") <- ns
   val
 }
--- inst/tinytest/standardize.R
+++ inst/tinytest/standardize.R
@@ -3,25 +3,50 @@ n <- 20
 p <- 5
 l <- 5
 
-# std standardizes correctly"
+# std standardizes correctly
 X <- matrix(rnorm(n*p),ncol=p)
 XX <- std(X)
 expect_equivalent(apply(XX, 2, mean), rep(0, ncol(XX)))
 expect_equivalent(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
 
-# std works on data frames"
+# std works on data frames
 XX <- std(airquality)
 expect_equivalent(apply(XX, 2, mean), rep(0, ncol(XX)))
 expect_equivalent(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
 
-# std works with integer matrix"
+# std works with integer matrix
 X <- matrix(as.integer(rpois(n*p, 1)), ncol=p)
 XX <- std(X)
 expect_equivalent(apply(XX, 2, mean), rep(0, ncol(XX)))
 expect_equivalent(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
 
-# std works with vector input"
+# std works with vector input
 x <- rnorm(n)
 XX <- std(x)
 expect_equivalent(apply(XX, 2, mean), rep(0, ncol(XX)))
 expect_equivalent(apply(XX, 2, crossprod), rep(nrow(XX), ncol(XX)))
+
+# std works with new data
+X <- matrix(rnorm(50), 10, 5)
+S <- std(X)
+XX <- matrix(rnorm(50), 10, 5)
+SS <- std(S, X)
+expect_true(all.equal(colMeans(SS), rep(0, ncol(SS)), tol=0.1))
+expect_true(all.equal(colMeans(SS^2), rep(1, ncol(SS)), tol=0.1))
+SS <- std(S, XX)
+expect_inherits(all.equal(colMeans(SS), rep(0, ncol(SS)), tol=0.1), 'character')
+expect_inherits(all.equal(colMeans(SS^2), rep(1, ncol(SS)), tol=0.1), 'character')
+
+# Predictions all agree
+y <- rnorm(10)
+fit <- ncvreg(X, y)
+P1 <- predict(fit, X)
+fit <- ncvreg(S, y)
+P2 <- predict(fit, S)
+expect_equivalent(P1, P2)
+y <- rnorm(10)
+fit <- ncvreg(X, y)
+P1 <- predict(fit, XX)
+fit <- ncvreg(S, y)
+P2 <- predict(fit, SS)
+expect_equivalent(P1, P2)
