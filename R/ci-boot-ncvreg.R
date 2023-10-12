08cbc3ccdbd6b853005f6e008936d310fc083260
Logan Harris <lharris4211998@gmail.com>
2023-10-12 10:06:29 -0500

    Adding initial functions for bootstrap

---
+++ R/ci-boot-ncvreg.R
@@ -0,0 +1,32 @@
+#' Title
+#'
+#' @param eb_boot 
+#' @param quiet 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ci.boot.ncvreg <- function(eb_boot, quiet = FALSE) {
+  
+  rm_lower <- apply(eb_boot[["lowers"]], 2, function(x) sum(is.na(x))); names(rm_lower) <- names(eb_boot$estimates)
+  rm_upper <- apply(eb_boot[["uppers"]], 2, function(x) sum(is.na(x))); names(rm_upper) <- names(eb_boot$estimates)
+  
+  rm_lower <- rm_lower[rm_lower != 0]
+  rm_upper <- rm_upper[rm_upper != 0]
+  
+  if (length(rm_lower) > 0 ) {
+    if (!quiet) {
+      print(paste0(length(rm_lower), " total variables with NA entries, summary: "))
+      print(summary(rm_lower))
+    } 
+  }
+  
+  lowers <- apply(eb_boot[["lowers"]], 2, mean, na.rm = TRUE)
+  uppers <- apply(eb_boot[["uppers"]], 2, mean, na.rm = TRUE)
+  
+  ci_info <- data.frame(estimate = eb_boot[["estimates"]], variable = names(eb_boot[["estimates"]]), lower = lowers, upper = uppers, method = "Lasso Boot")
+  
+  return(ci_info)
+  
+}
---
+++ R/plot-boot-ncvreg.R
@@ -0,0 +1,26 @@
+#' Title
+#'
+#' @param eb_boot 
+#' @param n 
+#' @param quiet 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot.boot.ncvreg <- function(eb_boot, n = 30, quiet = TRUE) {
+  
+  plot_res <- ci.boot.ncvreg(eb_boot, quiet = quiet) %>%
+    dplyr::arrange(desc(abs(estimate))) %>%
+    head(n)
+  
+  plot_res$variable <- factor(plot_res$variable, levels = rev(plot_res$variable))
+  
+  plot_res %>%
+    ggplot() +
+    geom_errorbar(aes(xmin = lower, xmax = upper, y = variable)) +
+    geom_point(aes(x = estimate, y = variable)) +
+    theme_bw() +
+    labs(y = "Variable", x = "Estimate")
+  
+}
