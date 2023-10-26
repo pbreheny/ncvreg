38b46116f11e8a7fd64feb1d60f4cf538b9266bc
Logan Harris <lharris4211998@gmail.com>
2023-10-26 14:38:23 -0500

    Numerical stability

--- R/boot-ncvreg.R
+++ R/boot-ncvreg.R
@@ -13,7 +13,7 @@
 #'
 #' @param X The design matrix, without an intercept, as in \code{ncvreg}
 #' @param y The response vector, as in \code{ncvreg}
-#' @param cvncvreg Instead of \code{X} and \code{y}, an object of type 
+#' @param cv_fit Instead of \code{X} and \code{y}, an object of type 
 #' \code{cv.ncvreg} with \code{returnX = TRUE} and \code{penalty = "lasso"}
 #' @param lambda A positive scalar value of type numeric, if left unspecified, selected using \code{cv.ncvreg}
 #' @param sigma2 A known / estimated value for the variance used in the Laplace prior and Normal likelihood, if left unspecified, selected using \code{cv.ncvreg} only if \code{lambda} is also left as unspecified
@@ -32,9 +32,6 @@
 #' like to specify a different seed to \code{cv.ncvreg}, they should manually
 #' specify the call to \code{cv.ncvreg} in the arguments.
 #' @param returnCV Whether to return \code{cv.ncvreg} object, \code{FALSE} by default.
-#' @param trace If set to TRUE, inform the user of progress by displaying 
-#' progress through bootstrapping and by announcing the beginning of each CV 
-#' fold if \code{cv.ncvreg} is ran.  Default is FALSE.
 #'
 #' @return An object with S3 class \code{boot.ncvreg} containing: \describe { 
 #' \item{lowers}{A \code{nboot} by \code{ncol(X)} matrix of the lower bounds
@@ -55,46 +52,47 @@
 #' 
 #' tmp <- boot.ncvreg(dat$X, dat$y)
 #' 
-#' ## Specifying cvncvreg
-#' tmp <- boot.ncvreg(cvncvreg = cv.ncvreg(dat$X, dat$y, penalty = "lasso", returnX = TRUE))
+#' ## Specifying cv_fit
+#' tmp <- boot.ncvreg(cv_fit = cv.ncvreg(dat$X, dat$y, penalty = "lasso", returnX = TRUE))
 #' 
 #' @export boot.ncvreg
-boot.ncvreg <- function(X, y, cvncvreg, lambda, sigma2, significance_level = 0.8, nboot = 100, ..., cluster, seed, returnCV=FALSE, trace=FALSE, verbose = TRUE) {
+boot.ncvreg <- function(X, y, cv_fit, lambda, sigma2, significance_level = 0.8, nboot = 100, ..., cluster, seed, returnCV=FALSE, verbose = TRUE, time = FALSE) {
   
-  ## name conflict: lambda -> removed from being able to specify own sequence fo cv.ncvreg as temp solution
-  ## Handle setting 2 seeds or resetting seeds? Don't really want to support this -> set seed one
-  ## Both can be circumvented by using cv.ncvreg route if desired
-  ## Best naming convention for cvncvreg?
-  ## Should warn / convex be saved by default? (no)
-  ## Match arguments (so they don't need to be named??)
-  ## NEED TO CHECK IF MISSING ONLY BEFORE I ACTIVELY ASSIGN SOMETHING
+  if (time) tic(msg = "Overall")
   
-  if ((missing(X) | missing(y)) & (missing(cvncvreg) || class(cvncvreg) != "cv.ncvreg")) {
+  if (time) tic(msg = "Checks")
+  if ((missing(X) | missing(y)) & (missing(cv_fit) || class(cv_fit) != "cv.ncvreg")) {
     stop("Either X and y or an object of class cv.ncvreg must be supplied.")
   }
   
-  if (!missing(cvncvreg)) {
-    if (!all(c("X", "y") %in% names(cvncvreg$fit))) {
-      stop("fit object in cvncvreg missing X and y, please rerun cv.ncvreg with returnX = TRUE or supply X and y directly (without also specifying a cv.ncvreg object)") 
+  if (!missing(cv_fit)) {
+    if (!all(c("X", "y") %in% names(cv_fit$fit))) {
+      stop("fit object in cv_fit missing X and y, please rerun cv.ncvreg with returnX = TRUE or supply X and y directly (without also specifying a cv.ncvreg object)") 
     }
     
-    if(cvncvreg$fit$penalty != "lasso") {
-      stop(paste0("cv.ncvreg fit with ", cvncvreg$fit$penalty, " penalty, but only 'lasso' penalty is currently supported for boot.ncvreg"))
+    if(cv_fit$fit$penalty != "lasso") {
+      stop(paste0("cv.ncvreg fit with ", cv_fit$fit$penalty, " penalty, but only 'lasso' penalty is currently supported for boot.ncvreg"))
     }
     
-    if(cvncvreg$fit$family != "gaussian") {
-      stop(paste0("cv.ncvreg fit with ", cvncvreg$fit$family, " family, but only 'gaussian' family is currently supported for boot.ncvreg"))
+    if(cv_fit$fit$family != "gaussian") {
+      stop(paste0("cv.ncvreg fit with ", cv_fit$fit$family, " family, but only 'gaussian' family is currently supported for boot.ncvreg"))
     }
   }
   
-  if (!missing(y) & !missing(cvncvreg)) {
+  if (!missing(y) & !missing(cv_fit)) {
     warning("Ignoring supplied values of y, using y from supplied cv.ncvreg object")
   }
   
-  if (!missing(X) & !missing(cvncvreg)) {
-    if ("X" %in% names(cvncvreg$fit)) {
+  if (!missing(X) & !missing(cv_fit)) {
+    if ("X" %in% names(cv_fit$fit)) {
       warning("Ignoring supplied values of X, using X from supplied cv.ncvreg object") 
     }
+  }
+  
+  if (!missing(X)) {
+    rescale_original <- FALSE
+  } else {
+    rescale_original <- TRUE
   }
   
   # Coercion
@@ -119,7 +117,7 @@ boot.ncvreg <- function(X, y, cvncvreg, lambda, sigma2, significance_level = 0.8
   }
   
   args <- list(...)
-  if (length(args) > 0 & !missing(cvncvreg)) {
+  if (length(args) > 0 & !missing(cv_fit)) {
     warning("Additional arguments are ignored when cv.ncvreg object supplied")
   }
   
@@ -128,7 +126,7 @@ boot.ncvreg <- function(X, y, cvncvreg, lambda, sigma2, significance_level = 0.8
   }
   
   
-  cv.args <- args[names(args) %in% c("nfolds", "fold", "returnY")]
+  cv.args <- args[names(args) %in% c("nfolds", "fold", "returnY", "trace")]
   ncvreg.args <- args[names(args) %in% c("lambda.min", "nlambda", "eps", "max.iter", "dfmax")]
   if ("penalty.factor" %in% names(args)) {
     stop("Sorry, specification of alternative penality factors is not yet supported")
@@ -151,15 +149,11 @@ boot.ncvreg <- function(X, y, cvncvreg, lambda, sigma2, significance_level = 0.8
     warning(paste0("Ignoring argument(s) ", paste0(names(args)[names(args) %in% c("gamma", "alpha")], collapse = " and "), ", not used for lasso penalty"))
   }
   
-  if (!missing(X)) {
-    rescale_original <- FALSE
-  } else {
-    rescale_original <- TRUE
-  }
+  if (time) toc()
   
   original_coefs <- NULL
   ## Will select lambda, won't estimate sigma^2 without selecting lambda
-  if (missing(cvncvreg)) {
+  if (missing(cv_fit)) {
     if (missing(lambda) | missing(sigma2)) {
       if (missing(lambda) & missing(sigma2)) {
         if (verbose) message("Using cross validation to select lambda and estimate variance")
@@ -169,52 +163,58 @@ boot.ncvreg <- function(X, y, cvncvreg, lambda, sigma2, significance_level = 0.8
         message("Using cross validation to estimate variance at supplied value of lambda using linear interpolation")
       }
       
-      cv.args$trace <- trace
+      if (time) tic(msg = "Cross Validation")
       cv.args$penalty <- "lasso"
       cv.args$X <- X
       cv.args$y <- y
       if (!missing(cluster)) cv.args <- cluster
-      cvncvreg <- do.call("cv.ncvreg", c(cv.args, ncvreg.args))
-      if (missing(lambda)) lambda <- cvncvreg$lambda.min
+      cv_fit <- do.call("cv.ncvreg", c(cv.args, ncvreg.args))
+      if (missing(lambda)) lambda <- cv_fit$lambda.min
       if (missing(sigma2)) {
-        if (max(cvncvreg$lambda) < lambda | min(cvncvreg$lambda) > lambda) stop("Supplied lambda value is outside the range of the model fit.")
+        if (max(cv_fit$lambda) < lambda | min(cv_fit$lambda) > lambda) stop("Supplied lambda value is outside the range of the model fit.")
         ## Make note about linear interpolation (or in documentation)
-        ind <- stats::approx(cvncvreg$lambda, seq(cvncvreg$lambda), lambda)$y
+        ind <- stats::approx(cv_fit$lambda, seq(cv_fit$lambda), lambda)$y
         l <- floor(ind)
         r <- ceiling(ind)
         w <- ind %% 1
-        sigma2 <- (1-w)*cvncvreg$cve[l] + w*cvncvreg$cve[r]
+        sigma2 <- (1-w)*cv_fit$cve[l] + w*cv_fit$cve[r]
       }
-      original_coefs <- coef(cvncvreg$fit, lambda = lambda)[-1]
+      original_coefs <- coef(cv_fit$fit, lambda = lambda)[-1]
+      if (time) toc()
     } 
   } else {
+    if (time) tic(msg = "Setting arguments with supplied cv.ncvreg")
     if (!missing(sigma2) & verbose) message("Overriding variance estimate in cv.ncvreg object with user specified value for sigma2.")
     if (!missing(lambda) & verbose) message("Overriding selected lambda in cv.ncvreg object with user specified value for lambda.")
     if (!missing(lambda) & missing(sigma2)) warning("Estimating variance using CV but using user specified value of lambda. Estimated variance corresponds to interpolated CVE for supplied value of lambda.")
-    if (missing(lambda)) lambda <- cvncvreg$lambda.min
+    if (missing(lambda)) lambda <- cv_fit$lambda.min
     if (missing(sigma2)) {
-      if (max(cvncvreg$lambda) < lambda | min(cvncvreg$lambda) > lambda) stop("Supplied lambda value is outside the range of the model fit.")
+      if (max(cv_fit$lambda) < lambda | min(cv_fit$lambda) > lambda) stop("Supplied lambda value is outside the range of the model fit.")
       ## Make note about linear interpolation (or in documentation)
-      ind <- stats::approx(cvncvreg$lambda, seq(cvncvreg$lambda), lambda)$y
+      ind <- stats::approx(cv_fit$lambda, seq(cv_fit$lambda), lambda)$y
       l <- floor(ind)
       r <- ceiling(ind)
       w <- ind %% 1
-      sigma2 <- (1-w)*cvncvreg$cve[l] + w*cvncvreg$cve[r]
+      sigma2 <- (1-w)*cv_fit$cve[l] + w*cv_fit$cve[r]
     }
-    X <- cvncvreg$fit$X
-    y <- cvncvreg$fit$y
-    original_coefs <- coef(cvncvreg$fit, lambda = lambda)[-1]
+    X <- cv_fit$fit$X
+    y <- cv_fit$fit$y
+    original_coefs <- coef(cv_fit$fit, lambda = lambda)[-1]
+    if (time) toc()
   }
   
   if (is.null(original_coefs)) {
+    if (time) tic(msg = "Getting posterior mode")
     coef.args <- ncvreg.args
     coef.args$X <- X
     coef.args$y <- y
     coef.args$penalty <- "lasso"
     fit <- do.call("ncvreg", coef.args)
     original_coefs <- coef(fit, lambda = lambda)[-1]
+    if (time) toc()
   }
-  
+
+  if (time) tic(msg = "Bootstrapping")  
   lowers <- uppers <- modes <- matrix(nrow = nboot, ncol = ncol(X))
 
   if (!missing(cluster)) {
@@ -234,14 +234,20 @@ boot.ncvreg <- function(X, y, cvncvreg, lambda, sigma2, significance_level = 0.8
     uppers[i,] <- res$uppers
     modes[i,] <- res$modes
   }
+  if (time) toc()
   
   val <- list(lowers = lowers, uppers = uppers, modes = modes, estimates = original_coefs)
   
-  if (returnCV) val$cv.ncvreg <- cvncvreg
-  structure(val, class="boot.ncvreg")
-}
-bootf <- function(XX, y, lambda, sigma2, significance_level = .8, ncvreg.args, rescale_original = TRUE) {
+  if (returnCV) val$cv.ncvreg <- cv_fit
   
+  if (time) toc() ## For overall
+  structure(val, class="boot.ncvreg")
+  
+}
+bootf <- function(XX, y, lambda, sigma2, significance_level = .8, ncvreg.args, rescale_original = TRUE, time = FALSE) {
+  
+  if (time) tic(msg = "Overall - Bootstrap")
+  if (time) tic(msg = "Prep")
   if (missing(ncvreg.args)) {
     ncvreg.args <- list()
   }
@@ -252,11 +258,15 @@ bootf <- function(XX, y, lambda, sigma2, significance_level = .8, ncvreg.args, r
   n <- length(y)
   
   modes <- uppers <- lowers <- numeric(p)
+  if (time) toc()
   
+  if (time) tic(msg = "Sample")
   idx_new <- sample(1:n, replace = TRUE)
   ynew <- y[idx_new]
   xnew <- ncvreg::std(XX[idx_new,,drop=FALSE])
+  if (time) toc()
   
+  if (time) tic(msg = "Lambda Sequence")
   lambda_max <- max(apply(xnew, 2, find_thresh, ynew))
   lambda_min <- lambda - lambda / 100 ## set min to be slightly smaller
   if (lambda_min > lambda_max | lambda > lambda_max) {
@@ -266,111 +276,84 @@ bootf <- function(XX, y, lambda, sigma2, significance_level = .8, ncvreg.args, r
   ## Could use better logic to speed up
   nlambda <- ifelse(!is.null(ncvreg.args$nlambda), ncvreg.args$nlambda, 100)
   lambda_seq <- 10^(seq(log(lambda_max, 10), log(lambda_min, 10), length.out = nlambda))
+  if (time) toc()
   
+  if (time) tic(msg = "Fit ncvreg")
   ncvreg.args$X <- xnew
   ncvreg.args$y <- ynew
   ncvreg.args$penalty <- "lasso"
   ncvreg.args$lambda <- lambda_seq
+  
   ## Ignores user specified lambda.min and nlambda
-  fit <- do.call("ncvreg", ncvreg.args[!(ncvreg.args %in% c("lambda.min", "nlambda"))])
+  fit <- do.call("ncvreg", ncvreg.args[!(names(ncvreg.args) %in% c("lambda.min", "nlambda"))])
   
   coefs <- coef(fit, lambda = lambda)
+  if (time) toc()
   
+  if (time) tic(msg = "Compute Posterior")
   ns_index <- attr(xnew, "nonsingular")
   modes <- coefs[-1] ## Coefs only returned for nonsingular columns of X
   
   partial_residuals <-  ynew - (coefs[1] + as.numeric(xnew %*% modes) - (xnew * matrix(modes, nrow=nrow(xnew), ncol=ncol(xnew), byrow=TRUE)))
   
   z <- (1/n)*colSums(xnew * partial_residuals)
-  
-  # print(z)
-  
-  r <- (1/n)*colSums(partial_residuals * partial_residuals)
   se <- sqrt(sigma2 / n)
   
+  ## Tails I am transferring on to (log probability in each tail)
   obs_lw <- pnorm(0, z + lambda, se, log.p = TRUE)
   obs_up <- pnorm(0, z - lambda, se, lower.tail = FALSE, log.p = TRUE)
   
-  
-  ## Find the zero density for each tail
+  ## Find the log density at zero for each tail (This needs to be finite)
   dens0_lw <- dnorm(0, z + lambda, se, log = TRUE)
   dens0_up <- dnorm(0, z - lambda, se, log = TRUE)
   
-  dens_adjust_lw <- den0_lw - dens0_up
-  print(dens_adjust)
-  ## Can adjust this to avoid dividing by zero
-  dens_adjust_up <- ifelse(dens_adjust_lw < 0, 0, dens_adjust_lw)
-  dens_adjust_lw <- ifelse(dens_adjust_lw > 0, 0, -dens_adjust_lw) ## Make sure these are correct on log scale
+  ## The difference in the tails log density at 0 I am transferring to
+  ## Since the posterior must be continuous at 0, these tell me how 
+  ## the probabilities above need to be adjusted to get the correct 
+  ## ratio of probabilities in each of the tails of the posterior
+  dens_adjust <- dens0_lw - dens0_up
   
+  ## I want to just adjust one of the tails, and I want to make the log probability
+  ## less negative, so this determines how to make that adjustment and applies it
+  dens_adjust_lw <- ifelse(dens_adjust > 0, 0, -dens_adjust)
+  dens_adjust_up <- ifelse(dens_adjust < 0, 0, dens_adjust)
+  
+  ## These two probabilities now contain the correct ratio of the lower and upper tails of
+  ## the posterior
   obs_p_lw <- obs_lw + dens_adjust_lw
   obs_p_up <- obs_up + dens_adjust_up
   
-  ## Can put a branch here that if the difference in logs is more than a value that relates to the significance
-  ## level set that we can avoid sme computions (which my throw errors but aren't needed)
+  ## But I need to use this to find the proportion of each to the overall probability
+  frac_lw_log <- ifelse(is.infinite(exp(obs_p_lw - obs_p_up)), 0, obs_p_lw - obs_p_up - log(1 + exp(obs_p_lw - obs_p_up)))
+  frac_up_log <- ifelse(is.infinite(exp(obs_p_up - obs_p_lw)), 0, obs_p_up - obs_p_lw - log(1 + exp(obs_p_up - obs_p_lw)))
   
+  ## Check sum to 1
   
-  frac_lw_log <- obs_p_lw - obs_p_up - log(1 + exp(obs_p_lw - obs_p_up))
-  frac_up_log <- obs_p_up - obs_p_lw - log(1 + exp(obs_p_up - obs_p_lw))
-    
-  
-  ## Need to use original obs for the adjustment I think
-  
-  # print(pnorm(0, z + lambda, se, log.p = TRUE))
-  # print(pnorm(0, z - lambda, se, lower.tail = FALSE, log.p = TRUE))
-  
-  # logC <- 2*n*z*lambda / sigma2
-  # C <- exp(2*n*z*lambda / sigma2)
-  
-  # print(exp(n*z*lambda / sigma2))
-  # print(exp(-n*z*lambda / sigma2))
-  
-  # lower_adj <- exp(obs_lw) + exp(obs_up - logC)
-  # upper_adj <- exp(obs_up) + exp(obs_lw + logC)
-
-  # lower_adj <- obs_lw + obs_up*C^(-1)
-  # upper_adj <- obs_up + obs_lw*C
-  
-  
-
-  
-  # lwr <- (sqrt(2*sigma2*pi / n))*(sqrt(sigma2*2*pi)^(-n))*(n*lambda / (2*sigma2))*exp(-(n/(2*sigma2))*(r - (z + lambda)^2))
-  # upr <- (sqrt(2*sigma2*pi / n))*(sqrt(sigma2*2*pi)^(-n))*(n*lambda / (2*sigma2))*exp(-(n/(2*sigma2))*(r - (z - lambda)^2))
-  
-  # C <- exp(n*z*lambda / sigma2)
-  # t <- obs_lw * C + obs_up * C^(-1)
-  # lower_adj <- t / C 
-  # upper_adj <- t / (C^-1)
-  
+  ## Unlikely to fail at this point, possible but would already need to be
+  ## very near limit with dnorm above
   lower <- ifelse(
-    prop_lw >= lower_p,
-    qnorm(log(lower_p) + log(lower_adj), z + lambda, se, log.p = TRUE),
-    qnorm(log(upper_p) + log(upper_adj), z - lambda, se, lower.tail = FALSE, log.p = TRUE)
+    frac_lw_log >= log(lower_p),
+    qnorm(log(lower_p) + obs_lw - frac_lw_log, z + lambda, se, log.p = TRUE),
+    qnorm(log(upper_p) + obs_up - frac_up_log, z - lambda, se, lower.tail = FALSE, log.p = TRUE)
   )
   upper <- ifelse(
-    prop_lw >= upper_p,
-    qnorm(log(upper_p) + log(lower_adj), z + lambda, se, log.p = TRUE),
-    qnorm(log(lower_p) + log(upper_adj), z - lambda, se, lower.tail = FALSE, log.p = TRUE)
+    frac_lw_log >= log(upper_p),
+    qnorm(log(upper_p) + obs_lw - frac_lw_log, z + lambda, se, log.p = TRUE),
+    qnorm(log(lower_p) + obs_up - frac_up_log, z - lambda, se, lower.tail = FALSE, log.p = TRUE)
   )
+  if (time) toc()
   
-  lower <- ifelse(
-    prop_lw >= lower_p,
-    qnorm(log(lower_p) + log(lower_adj), z + lambda, se, log.p = TRUE),
-    qnorm(log(upper_p) + log(upper_adj), z - lambda, se, lower.tail = FALSE, log.p = TRUE)
-  )
-  upper <- ifelse(
-    prop_lw >= upper_p,
-    qnorm(log(upper_p) + log(lower_adj), z + lambda, se, log.p = TRUE),
-    qnorm(log(lower_p) + log(upper_adj), z - lambda, se, lower.tail = FALSE, log.p = TRUE)
-  )
-
-  
+  if (time) tic(msg = "Rescale")
   rescale <- (attr(xnew, "scale")[ns_index])^(-1)
   if (!is.null(attr(XX, "scale")) & rescale_original) {
     rescaleX <-  (attr(XX, "scale")[ns_index])^(-1)
   } else {
     rescaleX <- 1
   }
+  if (time) toc()
   
+  
+  if (time) tic(msg = "Return result")
   lowers[ns_index] <- (lower * rescale) * rescaleX
   uppers[ns_index] <- (upper * rescale) * rescaleX
   modes[ns_index] <- (modes * rescale) * rescaleX
@@ -380,6 +363,8 @@ bootf <- function(XX, y, lambda, sigma2, significance_level = .8, ncvreg.args, r
   
   ret <- list(lowers, uppers, modes)
   names(ret) <- c("lowers", "uppers", "modes")
+  if (time) toc()
+  if (time) toc()
   return(ret)
   
 }
