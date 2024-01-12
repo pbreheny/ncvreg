91b10ff90dc241ed13756e8b519d3f8fe943b737
Logan Harris <lharris4211998@gmail.com>
2024-01-11 20:00:09 -0600

    Alternative sampling method BCA interval implimentation

--- R/ci-boot-ncvreg.R
+++ R/ci-boot-ncvreg.R
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-ci.boot.ncvreg <- function(eb_boot, quiet = FALSE, method = "quantile", alpha = 0.2) {
+ci.boot.ncvreg <- function(eb_boot, quiet = FALSE, method = "quantile", alpha = 0.2, original_data = NULL) {
   
   if (method == "quantile") {
     all_draws <- eb_boot[["draws"]]
@@ -20,7 +20,7 @@ ci.boot.ncvreg <- function(eb_boot, quiet = FALSE, method = "quantile", alpha = 
     ci_info <- data.frame(
       "estimate" = eb_boot$estimates,
       "variable" = names(eb_boot$estimates),
-      "ci" = t(sapply(1:ncol(eb_boot$draws), function(x) BCa_ci(eb_boot$draws[,x], eb_boot$estimates[x], alpha = alpha))),
+      "ci" = BCa_ci(eb_boot$draws, eb_boot$estimates, original_data = original_data, lambda = eb_boot$lambda, sigma2 = eb_boot$sigma2, alpha = alpha),
       "method" = "Lasso Boot"
     )
     
@@ -32,16 +32,19 @@ ci.boot.ncvreg <- function(eb_boot, quiet = FALSE, method = "quantile", alpha = 
   
 }
 
-BCa_ci <- function(bootstrap_samples, original_estimate, alpha = 0.05) {
+BCa_ci <- function(bootstrap_samples, original_estimate, original_data, lambda, sigma2, alpha = 0.05) {
+  
   # Calculate the number of bootstrap samples
-  n <- length(bootstrap_samples)
+  n <- nrow(bootstrap_samples)
   
   # Calculate bias correction (z0)
-  z0 <- qnorm(mean(bootstrap_samples < original_estimate))
+  quant <- sapply(1:ncol(bootstrap_samples), function(x) mean(bootstrap_samples[,x] < original_estimate[x]))
+  quant <- ifelse(quant == 1, 1 - (1/n), quant)
+  quant <- ifelse(quant == 0, 1/n, quant)
+  z0 <- qnorm(quant)
   
-  # Compute acceleration (acc)
-  j <- jackknife(bootstrap_samples, original_estimate)
-  acc <- sum((mean(j) - j)^3) / (6 * sum((mean(j) - j)^2)^1.5)
+  # acc
+  acc <- jacknife_acc(original_data, lambda, sigma2)
   
   # Adjust alpha for two-tailed test
   alpha1 <- alpha / 2
@@ -50,20 +53,35 @@ BCa_ci <- function(bootstrap_samples, original_estimate, alpha = 0.05) {
   # Compute adjusted percentiles
   p1 <- pnorm(z0 + (z0 + qnorm(alpha1)) / (1 - acc * (z0 + qnorm(alpha1))))
   p2 <- pnorm(z0 + (z0 + qnorm(alpha2)) / (1 - acc * (z0 + qnorm(alpha2))))
+  print(p1)
+  print(p2)
   
   # Calculate BCa confidence interval
-  bca_ci <- quantile(bootstrap_samples, c(p1, p2))
+  bca_ci_lower <- sapply(1:ncol(bootstrap_samples), function(x) quantile(bootstrap_samples[,x], p1[x]))
+  bca_ci_upper <- sapply(1:ncol(bootstrap_samples), function(x) quantile(bootstrap_samples[,x], p2[x]))
+  bca_ci <- cbind(bca_ci_lower, bca_ci_upper)
+  print(bca_ci)
   
   return(bca_ci)
 }
 
-jackknife <- function(bootstrap_samples, original_estimate) {
+acceleration <- function(j) {
+  return(sum((mean(j) - j)^3) / (6 * sum((mean(j) - j)^2)^1.5))
+}
+
+jacknife_acc <- function(original_data, lambda, sigma2) {
+  
   # Compute jackknife estimates
-  n <- length(bootstrap_samples)
+  n <- nrow(original_data$X)
   jk_estimates <- numeric(n)
+  
+  # jk samples
+  jk_samples <- matrix(nrow = n, ncol = ncol(original_data$X))
   for (i in 1:n) {
-    jk_sample <- bootstrap_samples[-i]
-    jk_estimates[i] <- mean(jk_sample)
+    jk_samples[i,] <- bootf_nosample(original_data$X, original_data$y, lambda = lambda, sigma2 = sigma2)$draws
   }
-  return(jk_estimates)
+  
+  acc <- apply(jk_samples, 2, acceleration)
+  
+  return(acc)
 }
