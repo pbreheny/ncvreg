0aebfd3f5bda1ad3bbfc45d84e4fb1657cd5f511
Logan Harris <lharris4211998@gmail.com>
2024-05-19 20:10:03 -0500

    Update plotting method

--- R/plot-boot-ncvreg.R
+++ R/plot-boot-ncvreg.R
@@ -8,17 +8,24 @@
 #' @export
 #'
 #' @examples
-plot.boot.ncvreg <- function(eb_boot, n = 30, alpha = 0.2, original_order = FALSE, quiet = TRUE) {
+plot.boot.ncvreg <- function(eb_boot, n = 30, alpha = 0.2, original_order = FALSE, absolute_order = FALSE, quiet = TRUE) {
   
   plot_res <- ci.boot.ncvreg(eb_boot, quiet = quiet, alpha = alpha) 
   
   if (!original_order) {
     plot_res <- plot_res %>%
-      dplyr::arrange(desc(abs(estimate)))
+      dplyr::arrange(desc(abs(estimate))) 
   }
   
   plot_res <- plot_res %>%
     head(n)
+  
+  if (!original_order) {
+    if (!absolute_order) {
+      plot_res <- plot_res %>%
+        dplyr::arrange(desc(estimate)) 
+    }
+  }
   
   plot_res$variable <- factor(plot_res$variable, levels = rev(plot_res$variable))
   
