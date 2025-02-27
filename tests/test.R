48b6ad07dcb38eafbfb0a7c75fe42c6a1501b157
Patrick Breheny <patrick-breheny@uiowa.edu>
2025-02-27 11:25:01 -0600

    Tinkering with test coverage triggers

--- tests/test.R
+++ tests/test.R
@@ -1,5 +1,5 @@
 if (requireNamespace("tinytest", quietly=TRUE)) {
-  if (length(unclass(packageVersion("ncvreg"))[[1]]) == 4 | Sys.getenv('R_FORCE_TEST') == 'TRUE') {
+  if (identical(tolower(Sys.getenv("R_FORCE_TEST")), "true")) {
     tinytest::test_package("ncvreg", pattern="^[^_].*\\.[rR]$")
   }
 }
