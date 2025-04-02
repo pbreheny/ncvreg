d46a763a93c03da9ef14b456f1884236554df666
Patrick Breheny <patrick-breheny@uiowa.edu>
2025-04-02 13:15:20 -0500

    Starting to use lintr; some minor linting

--- inst/tinytest/auc.R
+++ inst/tinytest/auc.R
@@ -1,14 +1,14 @@
 if (interactive()) library(tinytest)
-library(survival, quietly=TRUE)
+library(survival, quietly = TRUE)
 
 # The quantities here are not exactly the same thing (one is in-sample, the
 # other is out-of-sample), but they should be close
 n <- 500
-X <- matrix(rnorm(n*10), n, 10)
-y <- Surv(rexp(n, rate=exp(X[,1])), rbinom(n, 1, prob=0.8))
-cvfit <- cv.ncvsurv(X, y, lambda.min=0, returnY=TRUE)
+x <- matrix(rnorm(n * 10), n, 10)
+y <- Surv(rexp(n, rate = exp(X[, 1])), rbinom(n, 1, prob = 0.8))
+cvfit <- cv.ncvsurv(x, y, lambda.min = 0, returnY = TRUE)
 a <- AUC(cvfit)
 a[length(a)]
-fit <- coxph(y~X)
+fit <- coxph(y ~ x)
 b <- concordancefit(y, -predict(fit))$concordance
-expect_equivalent(a[length(a)], b, tol=0.02)
+expect_equivalent(a[length(a)], b, tol = 0.02)
--- inst/tinytest/binomial.R
+++ inst/tinytest/binomial.R
@@ -1,70 +1,74 @@
 suppressPackageStartupMessages(library(glmnet))
 
 # ncvreg works for logistic regression
-X <- matrix(rnorm(500),ncol=10)
+x <- matrix(rnorm(500), ncol = 10)
 b <- rnorm(10)
-y <- rnorm(X%*%b) > 0
-beta <- glm(y~X,family="binomial")$coef
-fit <- ncvreg(X, y, lambda=1:0, family="binomial", penalty="SCAD", eps=.0001)
-scad <- coef(ncvreg(X, y, lambda=1:0, family="binomial", penalty="SCAD", eps=.0001), which=2)
-mcp <- coef(ncvreg(X, y, lambda=1:0, family="binomial", penalty="MCP", eps=.0001), which=2)
-expect_equivalent(scad, beta,tolerance=.01)
-expect_equivalent(mcp, beta,tolerance=.01)
+y <- rnorm(x %*% b) > 0
+beta <- glm(y ~ x, family = 'binomial')$coef
+fit <- ncvreg(x, y, lambda = 1:0, family = 'binomial', penalty = 'SCAD', eps = 0.0001)
+scad <- ncvreg(x, y, lambda = 1:0, family = 'binomial', penalty = 'SCAD', eps = 0.0001) |>
+  coef(which = 2)
+mcp <- ncvreg(x, y, lambda = 1:0, family = 'binomial', penalty = 'MCP', eps = 0.0001) |>
+  coef(which = 2)
+expect_equivalent(scad, beta, tolerance = 0.01)
+expect_equivalent(mcp, beta, tolerance = 0.01)
 
 # ncvreg reproduces lasso: binomial
-nlasso <- coef(fit <- ncvreg(X, y, family="binomial", penalty="lasso"))
-plot(fit, log=TRUE)
-glasso <- as.matrix(coef(fit <- glmnet(X, y, family="binomial", lambda=fit$lambda)))
-plot(fit, "lambda")
-expect_equivalent(nlasso,  glasso, tolerance=.01)
+nlasso <- coef(fit <- ncvreg(x, y, family = 'binomial', penalty = 'lasso'))
+plot(fit, log = TRUE)
+glasso <- glmnet(x, y, family = 'binomial', lambda = fit$lambda) |>
+  coef() |>
+  as.matrix()
+plot(fit, 'lambda')
+expect_equivalent(nlasso,  glasso, tolerance = 0.01)
 
 # logLik() is correct: binomial
-fit.mle <- glm(y~X, family="binomial")
-fit <- ncvreg(X, y, lambda.min=0, family="binomial")
-expect_equivalent(logLik(fit)[100],  logLik(fit.mle)[1], tol= .001)
-expect_equivalent(AIC(fit)[100], AIC(fit.mle), tol= .001)
+fit_mle <- glm(y ~ x, family = 'binomial')
+fit <- ncvreg(x, y, lambda.min = 0, family = 'binomial')
+expect_equivalent(logLik(fit)[100],  logLik(fit_mle)[1], tol = 0.001)
+expect_equivalent(AIC(fit)[100], AIC(fit_mle), tol = 0.001)
 
 # ncvreg dependencies work: binomial
 
 # Predict
-p <- predict(fit, X, 'link')
-p <- predict(fit, X, 'response')
-p <- predict(fit, X, 'class')
-p <- predict(fit, X, 'coef')
-p <- predict(fit, X, 'vars')
-p <- predict(fit, X, 'nvars')
+p <- predict(fit, x, 'link')
+p <- predict(fit, x, 'response')
+p <- predict(fit, x, 'class')
+p <- predict(fit, x, 'coef')
+p <- predict(fit, x, 'vars')
+p <- predict(fit, x, 'nvars')
 
 # y logical
-fit <- ncvreg(X, y==1, lambda.min=0, family='binomial')
+fit <- ncvreg(x, y == 1, lambda.min = 0, family = 'binomial')
 
 # Summary
-summary(fit, which=10)
-summary(fit, lam=0.05)
+summary(fit, which = 10)
+summary(fit, lam = 0.05)
 
 # cv.ncvreg() options work for binomial
 n <- 200
 p <- 10
-X <- matrix(rnorm(n*p), ncol=p)
+x <- matrix(rnorm(n * p), ncol = p)
 b <- c(-3, 3, rep(0, 8))
-y <- rnorm(n, mean=X%*%b, sd=1) > 0.5
+y <- rnorm(n, mean = x %*% b, sd = 1) > 0.5
 
-par(mfrow=c(2,2))
-cvfit <- cv.ncvreg(X, y, family="binomial")
-plot(cvfit, type="all")
+par(mfrow = c(2, 2))
+cvfit <- cv.ncvreg(x, y, family = 'binomial')
+plot(cvfit, type = 'all')
 summary(cvfit)
-predict(cvfit, type="coefficients")
-predict(cvfit, type="vars")
-predict(cvfit, type="nvars")
-head(predict(cvfit, X=X, "link"))
-head(predict(cvfit, X=X, "response"))
-head(predict(cvfit, X=X, "class"))
+predict(cvfit, type = 'coefficients')
+predict(cvfit, type = 'vars')
+predict(cvfit, type = 'nvars')
+head(predict(cvfit, x = x, 'link'))
+head(predict(cvfit, x = x, 'response'))
+head(predict(cvfit, x = x, 'class'))
 
 b <- c(-3, 3, rep(0, 8))
-y <- rnorm(n, mean=X%*%b, sd=5) > 0.5
-cvfit <- cv.ncvreg(X, y, family="binomial")
-plot(cvfit, type="all")
+y <- rnorm(n, mean = x %*% b, sd = 5) > 0.5
+cvfit <- cv.ncvreg(x, y, family = 'binomial')
+plot(cvfit, type = 'all')
 
 b <- rep(0, 10)
-y <- rnorm(n, mean=X%*%b, sd=5) > 0.5
-cvfit <- cv.ncvreg(X, y, family="binomial")
-plot(cvfit, type="all")
+y <- rnorm(n, mean = x %*% b, sd = 5) > 0.5
+cvfit <- cv.ncvreg(x, y, family = 'binomial')
+plot(cvfit, type = 'all')
